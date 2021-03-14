c7e10be82f1675d000e3eb289fca1ee818fbd3ee
OgoO1 <ogooladosu@gmail.com>
2021-03-14 09:18:54 +0000

    Func

---
+++ analysis/scripts/project_functions.py
@@ -0,0 +1,9 @@
+def load_and_process(url_or_path_to_csv_file):
+    df = (
+        pd.read_csv(url_or_path_to_csv_file)
+        .rename(columns ={"bmi": "BMI"})
+        .dropna()
+     )
+    
+    return df
+load_and_process("Medical_Cost.csv")
